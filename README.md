f7b716bdc481bafaccf82163f1f475914dd42318
Som Tambe <somvt@iitk.ac.in>
2021-09-29 18:14:33 +0000

    First commmit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # cpy-ble-beacons
-CircuitPython BLE library for making it easy to work with BLE beacons on the nRF52840
+CircuitPython BLE library for making it easy to work with BLE beacons on the nRF52840 boards.
+
+Low level access to BLE payloads was quite difficult with the existing `adafruit_ble` library simply because it has enigmatic documentation, and rather than banging my head on the wall for understanding the library, I thought it would be better and quick to write methods using the `_bleio` low level Bluetooth API.
+
+This is purely for beaconing purposes, and I have not written anything related to GATT protocols yet. I may add some features related to this in the near future.
+
+Advertising Data elements available right now:
+- Flags
+- Shortened local names
+- Complete local names
+
+Plan to add:
+- Manufacturer specific payload
+- 16-bit UUIDs
+- 128-bit UUIDs
+
+I currently use this for my experiments, so it's not actually stable. Maybe sometime later I will put up some really nice docs for the library which will make it more usable.
---
+++ src/beacons.py
@@ -0,0 +1,92 @@
+import _bleio
+import adafruit_ble
+import board
+import digitalio
+import binascii
+import math
+
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising import *
+from _bleio import adapter
+from math import floor
+
+# specifically written for the Adafruit nRF52840 Bluefruit Feather 
+# 0x01 => flags
+# 0x02 => incomplete list of 16-bit UUIDs
+# 0x06 => incomplete list of 128-bit UUIDs
+# 0x08 => shortened local name
+# 0x09 => complete local name
+# 0xFF => manufacturer specific payload
+
+class AD:
+    # make your own Advertising Data packet.
+    def __init__(self, length, dtype, payload, endian='little'):
+        """
+        length: The length of the AD packet including the type byte.
+        dtype: A 1-byte string representing one the datatype of the payload as documented in the Bluetooth Generic Access Profile (GAP)
+        payload: The actual payload. The length of this should be equal to (length - 1).
+        endian: Endianness of the datatype. Specify if 'big' or 'little'. Default 'little'.
+
+        All of these should be in String format. For example, if your dtype is a flag, which would be 0x01, dtype='01'. 
+        Similarly for others too.
+        """
+        self.length = bytes(length, 'utf-8')
+        self.dtype = bytes(dtype, 'utf-8')
+        self.payload = bytes(payload, 'utf-8') # convert all these to byte format
+
+        self.l = int(self.length, 16) # integer length of (payload + dtype) in bytes
+        if endian not in ['big', 'litle']:
+            self.endian = 'little'
+        else:
+            self.endian = endian
+
+    def parse(self):
+        p_len = binascii.unhexlify(self.length)
+        p_dtype = binascii.unhexlify(self.dtype)
+        p_pload = binascii.unhexlify(self.payload)
+        return p_len, p_dtype, p_pload
+
+    def join(self):
+        l, dt, pl = self.parse() # in pure hex byte representation
+
+        if len(pl) > (self.l - 1):
+            pl = pl[:self.l - 1]
+        if len(pl) < (self.l - 1) and endian == 'little':
+            pl = '\x00' * (self.l - 1 - len(pl)) + pl
+        if len(pl) < (self.l - 1) and endian == 'big':
+            pl = pl + '\x00' * (self.l - 1 - len(pl))
+        
+        return (l + dt + pl)
+    
+    def get_AD(self):
+        return self.join()
+
+def Flag(flag='0x06'):
+    """
+    Get the AD for the Flag.
+    Default: 0x06
+    """
+    l = '02'
+    dt = '01'
+    f = flag.split('0x')[1]
+    return AD(l, dt, f).join()
+
+def ShortName(ShortName):
+    """
+    Get AD for the short name of device.
+    """
+    l = binascii.hexlify((len(ShortName)+1).to_bytes(1, 'little')).decode('utf-8') # converts the length to a hex representation in a byte in String format
+    pl = ''
+    for i in to_hex(bytes(ShortName, 'utf-8')).split(' '):
+        pl += i
+    return AD(l, '08', pl).join()
+
+def CompleteName(fullName):
+    """
+    Get AD for the complete name of the device.
+    """
+    l = binascii.hexlify((len(fullName)+1).to_bytes(1, 'little')).decode('utf-8') # converts the length to a hex representation in a byte in String format
+    pl = ''
+    for i in to_hex(bytes(fullName, 'utf-8')).split(' '):
+        pl += i
+    return AD(l, '09', pl).join()
